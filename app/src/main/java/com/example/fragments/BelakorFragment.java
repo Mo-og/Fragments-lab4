ee23a68eb5d830b5bf44b17374a4e1ba386d017d
Maksim <mgorbiik@mail.ru>
2021-04-06 15:09:16 +0300

    fragments - lab 4

---
+++ app/src/main/java/com/example/fragments/BelakorFragment.java
@@ -0,0 +1,24 @@
+package com.example.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+public class BelakorFragment extends Fragment {
+
+    public BelakorFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_base, container, false);
+        ImageView img = view.findViewById(R.id.image);
+        img.setImageResource(R.drawable.belakor);
+        return view;
+    }
+}
